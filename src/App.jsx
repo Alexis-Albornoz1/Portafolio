5bad371d9748e2ff3442280596708913ffe1ea35
alexis albornoz <L21010915@orizaba.tecnm.mx>
2025-10-22 18:09:26 -0600

    docs:actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Alexis Albornoz",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  email: "L21010915@orizaba.tecnm.mx",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
